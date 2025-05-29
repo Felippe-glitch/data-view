c2c1c17530ed94b65918a810d15598f5e03fca02
Felippe <felps.facul@gmail.com>
2025-05-29 15:13:11 -0300

    first commit

---
+++ src/test/java/com/dataview/DataviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
