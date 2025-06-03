293bc8cd55387aad37b5ec590095e71a19cdd1d2
Felippe <felps.facul@gmail.com>
2025-06-03 16:52:21 -0300

    feat: cria repository para User

---
+++ src/main/java/com/dataview/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dataview.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dataview.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{}
